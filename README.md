f558c78c61bf587af1ef8951e8ddfd6fb74246f0
Tyler <ty8bri@gmail.com>
2022-04-16 01:07:47 -0700

    feat(pip): localstack: so far choosing not to use brew

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Using chezmoi.io
 
 `brew install chezmoi` or via a curl script from chezmoi.io
+
+## TODO
+[ ] load oh-my-zsh git aliases correctly
--- run_update.sh
+++ run_update.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-pnpm add -g pnpm
+pnpm add -g pnpm neovim # nvim :checkhealth doesn't seem to reconize pnpm
+
+pip install -U localstack pynvim pip
